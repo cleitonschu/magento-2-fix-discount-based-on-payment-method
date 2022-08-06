5d41370cd75d226523d847ad18aaa092ccae19c4
cleitonschu <57007650+cleitonschu@users.noreply.github.com>
2022-08-06 19:44:23 -0300

    First commit

---
+++ app/code/Tweezer/FixDiscountPaymentMethod/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Tweezer_FixDiscountPaymentMethod',
+    __DIR__
+);
---
+++ app/code/Tweezer/FixDiscountPaymentMethod/view/frontend/requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+    map: {
+        '*': {
+            'Magento_Checkout/js/action/select-payment-method':
+                'Tweezer_FixDiscountPaymentMethod/js/action/select-payment-method'
+        }
+    }
+};
